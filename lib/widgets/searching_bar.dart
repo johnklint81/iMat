1895e1b82e85007072aa8a40b0670a060e21716f
Mindana2 <leomindana@hotmail.com>
2025-05-12 13:59:42 +0200

    .

--- lib/widgets/searching_bar.dart
+++ lib/widgets/searching_bar.dart
@@ -6,22 +6,24 @@ class SearchingBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
+      child: SizedBox(
         width: 800,
         child: SearchAnchor.bar(
           barHintText: 'Skriv här för att hitta en vara...',
           suggestionsBuilder: (BuildContext context, SearchController controller){
-            return List<ListTile>.generate(5, (int index){
-              final String item = 'item $index';
-              return ListTile(title: Text(item),
-                onTap: (){true;},
-          );
-        }
-      );
-    }
+            return <Widget>[];
+          }
               )
             )
           );
   }
  
 }
+
+//List<ListTile>.generate(5, (int index){
+            //  final String item = 'item $index';
+         //     return ListTile(title: Text(item),
+             //   onTap: (){true;},
+         // );
+       // }
+     // );
