4925ca4535f8e5a4babfbcabcd4735b83b68abfe
John Klint <gusjohn25@student.gu.se>
2025-05-19 21:38:24 +0200

    Annan färg på hem/favoriterknapparna

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -5,6 +5,7 @@ class AppTheme {
   static const Color backgroundSplashColor = Color(0x50FFC266);
   static const Color buttonColor1 = Color(0xFF4CAF50);  // Green
   static const Color buttonColor2 = Color(0xFFF57C00); // orange.shade700
+  static const Color buttonColor3 = Color(0xFFFFE3C2); // Hem/Favoriter-knappfärg
   static const Color borderColor = Colors.black12;
   static const double paddingSuperTiny = 2.0;
   static const double paddingTiny = 4.0;
--- lib/widgets/categories.dart
+++ lib/widgets/categories.dart
@@ -8,30 +8,45 @@ class CategoryListItem extends StatefulWidget {
   final String label;
   final VoidCallback onTap;
   final IconData? icon;
+  final Color? backgroundColor;
+  final bool bordered;
+
   const CategoryListItem({
     super.key,
     required this.label,
     required this.onTap,
     this.icon,
+    this.backgroundColor,
+    this.bordered = false,
   });
+
   @override
   State<CategoryListItem> createState() => _CategoryListItemState();
 }
 
 class _CategoryListItemState extends State<CategoryListItem> {
   bool _hovering = false;
+
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       onEnter: (_) => setState(() => _hovering = true),
-      onExit:  (_) => setState(() => _hovering = false),
+      onExit: (_) => setState(() => _hovering = false),
       child: Material(
-        color: _hovering ? AppTheme.buttonColor2 : Colors.transparent,
+        color: _hovering
+            ? AppTheme.buttonColor2
+            : (widget.backgroundColor ?? Colors.transparent),
         borderRadius: BorderRadius.circular(6),
         child: InkWell(
           onTap: widget.onTap,
           borderRadius: BorderRadius.circular(6),
-          child: Padding(
+          child: Container(
+            decoration: widget.bordered
+                ? BoxDecoration(
+              border: Border.all(color: Colors.black26),
+              borderRadius: BorderRadius.circular(6),
+            )
+                : null,
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
             child: Row(
               children: [
@@ -51,20 +66,31 @@ class _CategoryListItemState extends State<CategoryListItem> {
 
 class CategorySelector extends StatelessWidget {
   final void Function(String? category)? onCategorySelected;
+
   const CategorySelector({super.key, this.onCategorySelected});
 
   static const Map<String, List<ProductCategory>> groupedCategories = {
-    'Grönt':           [ ProductCategory.VEGETABLE_FRUIT, ProductCategory.ROOT_VEGETABLE,
-      ProductCategory.CABBAGE, ProductCategory.HERB, ProductCategory.POD ],
-    'Frukt':           [ ProductCategory.FRUIT, ProductCategory.BERRY, ProductCategory.CITRUS_FRUIT,
-      ProductCategory.EXOTIC_FRUIT, ProductCategory.MELONS ],
-    'Mejeriprodukter': [ ProductCategory.DAIRIES ],
-    'Fisk':            [ ProductCategory.FISH ],
-    'Bröd & Pasta':    [ ProductCategory.BREAD, ProductCategory.PASTA ],
-    'Potatis & Ris':   [ ProductCategory.POTATO_RICE ],
-    'Nötter & Sötsaker':[ ProductCategory.NUTS_AND_SEEDS, ProductCategory.SWEET ],
-    'Drycker':         [ ProductCategory.HOT_DRINKS, ProductCategory.COLD_DRINKS ],
-    'Mjöl, socker & salt':     [ ProductCategory.FLOUR_SUGAR_SALT ],
+    'Grönt': [
+      ProductCategory.VEGETABLE_FRUIT,
+      ProductCategory.ROOT_VEGETABLE,
+      ProductCategory.CABBAGE,
+      ProductCategory.HERB,
+      ProductCategory.POD
+    ],
+    'Frukt': [
+      ProductCategory.FRUIT,
+      ProductCategory.BERRY,
+      ProductCategory.CITRUS_FRUIT,
+      ProductCategory.EXOTIC_FRUIT,
+      ProductCategory.MELONS
+    ],
+    'Mejeriprodukter': [ProductCategory.DAIRIES],
+    'Fisk': [ProductCategory.FISH],
+    'Bröd & Pasta': [ProductCategory.BREAD, ProductCategory.PASTA],
+    'Potatis & Ris': [ProductCategory.POTATO_RICE],
+    'Nötter & Sötsaker': [ProductCategory.NUTS_AND_SEEDS, ProductCategory.SWEET],
+    'Drycker': [ProductCategory.HOT_DRINKS, ProductCategory.COLD_DRINKS],
+    'Mjöl, socker & salt': [ProductCategory.FLOUR_SUGAR_SALT],
   };
 
   @override
@@ -75,25 +101,34 @@ class CategorySelector extends StatelessWidget {
     return ListView(
       children: [
         const SizedBox(height: AppTheme.paddingTiny),
+
         CategoryListItem(
           label: "Hem",
           icon: Icons.home,
+          bordered: true,
+          backgroundColor: AppTheme.buttonColor3,
           onTap: () {
             handler.selectAllProducts();
             onCategorySelected?.call(null);
           },
         ),
+
         const SizedBox(height: AppTheme.paddingTiny),
+
         CategoryListItem(
           label: "Favoriter",
           icon: Icons.star,
+          bordered: true,
+          backgroundColor: AppTheme.buttonColor3,
           onTap: () {
             handler.selectFavorites();
             onCategorySelected?.call(null);
           },
         ),
+
         const Divider(color: Colors.black),
         const SizedBox(height: AppTheme.paddingTiny),
+
         CategoryListItem(
           label: "Alla Produkter",
           onTap: () {
@@ -101,8 +136,9 @@ class CategorySelector extends StatelessWidget {
             onCategorySelected?.call(null);
           },
         ),
+
         const SizedBox(height: AppTheme.paddingTiny),
-        // Now your grouped sidebar buttons:
+
         for (final entry in groupedCategories.entries)
           CategoryListItem(
             label: entry.key,
